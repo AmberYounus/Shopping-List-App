efad9d2a095c787f7b83c8164823501367b8731b
Amber Younus <amberyounus22@gmail.com>
2023-09-17 23:44:22 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React ,{ useState,useEffect } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
+
 
 const App = () =>{
-  const [items,setItems]=useState([
+  const [items,setItems] = useState([
     {itemName:"item 1" ,quantity:1,isSelected:false},
     {itemName:"item 2" ,quantity:2,isSelected:false},
     {itemName:"item 3" ,quantity:3,isSelected:false},
   ])
 
   const [inputValue,setInputValue] = useState('');
+  const [totalItemCount ,setTotalItemCount] = useState(6);
   
   const  toggleComplete=(index)=>{
     const newItems =[...items];
     newItems[index].isSelected = !newItems[index].isSelected
     setItems(newItems)
+  }
+  const handleAddButtonClick =()=>{
+    const newItem ={
+      itemName:inputValue,
+      quantity:1,
+      isSelected:false,
+
+    }
+    const newItems = [...items,newItem]
+      setItems(newItems)
+      setInputValue('')
+      totalCalculate()   
+
+  }
+  const totalCalculate =()=>{
+      const totalItemCount = items.reduce((total,item)=>{
+          return total+item.quantity;
+       }, 0 )
+
+      setTotalItemCount(totalItemCount);
   }
   return(
     <div className="app-bg">
@@ -21,6 +45,7 @@ const App = () =>{
         <div className="add-item-box">
           <input value={inputValue} className='add-item-input' placeholder='Add an item ...'
           onChange={(e)=>setInputValue(e.target.value)} />
+          <FontAwesomeIcon icon={faPlus} onClick={()=>handleAddButtonClick()}/>
         </div>
         <div className="item-list">
           {items.map((item,index)=>(
@@ -28,10 +53,12 @@ const App = () =>{
               <div className="item-name" onClick={()=>toggleComplete(index)}>
                 {item.isSelected ? (
                   <>
+                  <FontAwesomeIcon icon={faCheckCircle}/>
                   <span className='completed'>{item.itemName}</span>
                   </>
                 ):(
                   <>
+                  <FontAwesomeIcon icon={faCircle}/>
                   <span >{item.itemName}</span>
                   </>
                 )
